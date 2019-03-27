3bcdf40ed3f08213139a58695d4526acd3ff43f0
crh2302 <crh2302@rit.edu>
2019-03-27 18:02:17 -0400

    Reimplemented commands using os.call instead of using the sh library.

--- create_projects.py
+++ create_projects.py
@@ -1,10 +1,10 @@
-import sh
+import subprocess
 import os
 import csv
 
 #https://scitools.com/documents/manuals/html/understand/wwhelp/wwhimpl/js/html/wwhelp.htm
 
-
+FNULL = open(os.devnull, 'w')
 '''
 Clone repository
 Begin
@@ -12,15 +12,12 @@ Begin
 
 git_repository_url = "https://github.com/git/git.git"
 
-try:
-    print("Cloning git project from ",git_repository_url)
-    sh.git("clone", git_repository_url, _out=True)
-except sh.ErrorReturnCode_128 as e:
-    print("Destination path 'git' already exists and is not an empty directory")
-except Exception as e:
-    print(e)
-    exit(1)
-
+print("Cloning git project from ", git_repository_url)
+output = subprocess.getoutput(f"git clone {git_repository_url}")
+if(output == "fatal: destination path 'git' already exists and is not an empty directory."):
+    print("destination path 'git' already exists and is not an empty directory.")
+else:
+    print(output)
 '''
 Clone repository
 End
@@ -53,9 +50,10 @@ End
 Calculate metrics 
 Begin
 '''
-
-root_dir = "/home/test-generators/PycharmProjects/research/"
+#
+root_dir = "D:\capstone"
 repository_dir = os.path.join(root_dir, "git")
+# repository_dir = "D:\capstone\git"
 #current_commit = "f0ac6e39433c1d7e9339207aa4d01e9bf7a05b8a"
 
 project_languages = "c++"
@@ -96,23 +94,33 @@ with open(commits_to_analyse_file_name, 'r') as cvs_file:
                     # sh.git(f"--git-dir={git_dir}",
                     #              f"--work-tree={repository_dir}",
                     #              "reset", "--hard", current_commit,  _out=True)
-
-                    sh.git(f"--git-dir={git_dir}", f"--work-tree={repository_dir}",
-                           "checkout", current_commit, _out=True)
+                    subprocess.call(f"git --git-dir={git_dir} --work-tree={repository_dir} checkout {current_commit}",
+                                    shell=True)
+                    # sh.git(f"--git-dir={git_dir}", f"--work-tree={repository_dir}",
+                    #        "checkout", current_commit, _out=True)
                     print(f"Creating udb database for commit: {current_commit}")
-                    sh.und.create("-db", udb_commit_file_name, "-languages", project_languages)
+                    subprocess.call(f"und create -db {udb_commit_file_name} -languages {project_languages}",
+                                    shell=True)
+
+                    # sh.und.create("-db", udb_commit_file_name, "-languages", project_languages)
 
                     print(f"Adding files to commit: {current_commit}")
-                    sh.und.add("-db", udb_commit_file_name, repository_dir)
+                    subprocess.call(f"und add -db {udb_commit_file_name} {repository_dir}",
+                                    shell=True)
+                    ##sh.und.add("-db", udb_commit_file_name, repository_dir)
 
                     print(f"Running Analysis for commit: {current_commit} ...")
-                    sh.und.analyze("-db", udb_commit_file_name)
+                    subprocess.call(f"und analyze -db {udb_commit_file_name}",
+                                    shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
+                    #sh.und.analyze("-db", udb_commit_file_name)
 
                     print(f"Adding metrics to commit: {current_commit}")
-                    sh.und.settings(f"@{settings_file_name}", udb_commit_file_name)
+                    subprocess.call(f"und settings @{settings_file_name} {udb_commit_file_name}")
+
 
                     print("Calculating metrics and creating csv")
-                    sh.und.metrics(udb_commit_file_name)
+                    subprocess.call(f"und metrics {udb_commit_file_name}")
+                    #sh.und.metrics(udb_commit_file_name)
 
                     print(f"Removing {udb_commit_file_name}")
                     os.remove(udb_commit_file_name)
@@ -120,8 +128,8 @@ with open(commits_to_analyse_file_name, 'r') as cvs_file:
                     print(f"Moving {current_commit}.csv to metric folder")
                     os.rename(os.path.join(root_dir, f"{current_commit}.csv"),os.path.join(metrics_folder_dir, f"{current_commit}.csv"))
                     break
-                except sh.SignalException_SIGSEGV:
-                    print(f"ERROR: An sh.SignalException_SIGSEGV error occurred on commit {count}")
+                except OSError as e:
+                    print(f"ERROR: An sh.SignalException error occurred on commit {count}: {e}")
             #Gather metrics /End
 
 
